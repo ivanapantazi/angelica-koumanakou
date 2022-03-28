76299b00119dea7e00210efa76e009b5ddd575ca
Ioanna Pantazi <Ioanna.Pantazi@reedonline.co.uk>
2022-03-28 21:51:04 +0100

    Minor updates

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { FeaturedPosts } from "../sections/index";
 import { PostCard, Categories } from "../components";
 import { getPosts } from "../services";
 
-export default function Projects({ posts }) {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <FeaturedPosts />
